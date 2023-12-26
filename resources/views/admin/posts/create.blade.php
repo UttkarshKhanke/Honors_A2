80970bd83bbd3eda7d3b5d6492761195d3120363
UttkarshKhanke <uttkarsh.khanke@gmail.com>
2023-12-27 00:56:26 +0530

    Add CkEditor to Create Post template

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -4,13 +4,11 @@
     </x-slot>
     <form method="POST" action="{{ route('admin-posts-create') }}" enctype="multipart/form-data" class="p-3">
         @csrf
-
         <!-- Title -->
         <div class="p-2">
             <label for="name">{{ __('Title') }}</label>
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('title')" required autofocus autocomplete="title" />
         </div>
-
         <!-- Category -->
         <div class="p-2">
             <label for="category_id">{{ __('Category') }}</label>
@@ -24,19 +22,30 @@
 
         <!-- Content -->
         <div class="p-2">
-            <label for="content">{{ __('Content') }}</label>
-            <textarea id="content"
+            <label for="content1">{{ __('Content') }}</label>
+            <textarea id="content1"
                 class="block mt-1 w-full rounded"
-                name="content" :value="old('title')"
+                name="content"
                 rows="6"
                 required autofocus></textarea>
         </div>
-
         <x-form-errors />
-
         <div class="block p-2">
             <x-primary-button type="submit">Create</x-primary-button>
         </div>
 
     </form>
+    <x-slot name="scripts">
+        <script src="https://cdn.ckeditor.com/ckeditor5/40.1.0/classic/ckeditor.js"></script>
+    </x-slot>
+    <script>
+        ClassicEditor
+            .create( document.querySelector( '#content1' ) )
+            .then( editor => {
+                    console.log( editor );
+            } )
+            .catch( error => {
+                    console.error( error );
+            } );
+    </script>
 </x-admin-layout>
--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -8,8 +8,12 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @if(isset($scripts))
+        {{ $scripts }}
+        @endif
     </head>
     <body class="antialiased min-h-screen lg:flex" x-data="{open: false}">
         <nav
